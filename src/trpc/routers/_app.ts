4719923d780fb346a112737ecadfd61b217bdc1e
Mayank Chandel <chandelmayank580@gmail.com>
2026-08-24 14:48:35 +0530

    integrate the trpc with tanstack query and created initial health check endpoint

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,12 @@
+import { baseProcedure, createTRPCRouter } from '../init';
+
+export const appRouter = createTRPCRouter({
+    health: baseProcedure.query(async () => {
+        await new Promise((resolve) => setTimeout(resolve, 5000))
+
+        return { status: "ok" }
+    })
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
